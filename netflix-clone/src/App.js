bf9a57e576624b85ae1754d1f50834c4b3283106
yonatan1 <162271220+yonatan1@users.noreply.github.com>
2024-05-08 13:17:24 -0500

    Header and Fotter

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div>
-      <h1> Hello</h1>
+    <div className='App'>
+      < Home/>
     </div>
   );
 }
